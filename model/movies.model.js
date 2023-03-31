b4ada95db0147cee15861db7dcefce922391747a
sarafbrun <sarafuen.93@gmail.com>
2023-03-31 16:40:43 +0200

    arreglo funcion get movies

--- model/movies.model.js
+++ model/movies.model.js
@@ -1,7 +1,7 @@
 //Methods Library - MOVIES
 
 const getAll = () => {
-    return db.query('SELECT * FROM harry_potter.characters');
+    return db.query('SELECT * FROM harry_potter.movies');
 }
 
 module.exports = { getAll };
